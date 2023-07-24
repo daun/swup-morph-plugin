50cb45ebaacf343763a9ad26cd84ce0feeda90d6
Philipp Daun <post@philippdaun.net>
2023-07-24 21:26:00 +0200

    Refactor for recent core updates

--- src/index.js
+++ src/index.js
@@ -13,21 +13,30 @@ export default class SwupMorphPlugin extends Plugin {
 
 	constructor(options) {
 		super();
-
 		this.options = { ...this.defaults, ...options };
-
-		this.validateContainers = this.validateContainers.bind(this);
-		this.morphContainers = this.morphContainers.bind(this);
 	}
 
 	mount() {
-		this.swup.hooks.before('content:replace', this.validateContainers);
-		this.swup.hooks.on('content:replace', this.morphContainers);
+		this.before('content:replace', this.validateContainers, { priority: 1 });
+		this.on('content:replace', this.morphContainers);
 	}
 
-	unmount() {
-		this.swup.hooks.off('content:replace', this.validateContainers);
-		this.swup.hooks.off('content:replace', this.morphContainers);
+	validateContainers(visit) {
+    // Filter out containers that are already managed by the morph plugin
+		context.containers = context.containers.filter(selector => !this.options.containers.includes(selector));
+	}
+
+	morphContainers(visit, { page }) {
+		const containers = this.getContainers();
+		const newContainers = this.getNewContainers(page);
+		const callbacks = this.options.updateCallbacks || [];
+
+		containers.forEach(({ element }, index) => {
+			const { element: newElement } = newContainers[index];
+			if (element && newElement) {
+				morph(element, newElement, callbacks);
+			}
+		});
 	}
 
 	getContainers(doc = document) {
@@ -40,23 +49,5 @@ export default class SwupMorphPlugin extends Plugin {
 	getNewContainers({ html }) {
 		const doc = new DOMParser().parseFromString(html, 'text/html');
 		return this.getContainers(doc);
-	}
-
-	validateContainers(context) {
-    // Filter out containers that are already managed by the morph plugin
-		context.containers = context.containers.filter(selector => !this.options.containers.includes(selector));
-	}
-
-	morphContainers(context, { page }) {
-		const containers = this.getContainers();
-		const newContainers = this.getNewContainers(page);
-		const callbacks = this.options.updateCallbacks || [];
-
-		containers.forEach(({ element }, index) => {
-			const { element: newElement } = newContainers[index];
-			if (element && newElement) {
-				morph(element, newElement, callbacks);
-			}
-		});
 	}
 }
